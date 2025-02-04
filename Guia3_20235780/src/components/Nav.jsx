3f3b3f9d9f90f99ac7ff5d3da0affb2b34d8e048
diegoemz <73715674+diegoemz@users.noreply.github.com>
2025-02-04 09:08:09 -0600

    Creación de Guía 3

---
+++ Guia3_20235780/src/components/Nav.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export const Nav = ({ children }) => {
+    return (
+        <nav className="nav-bar">
+            {children}
+        </nav>
+    )
+}
+
+export function Logo() {
+    return (
+        <div className="logo">
+            <span role="img">🍿</span>
+            <h1>Palomitas de papel</h1>
+        </div>
+    )
+}
+
+export function Search({ query, setQuery }) {
+    return (
+        <input
+            className="search"
+            type="text"
+            placeholder="Buscar peliculas..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+        />
+    )
+}
+
+export function NumResult({ movies }) {
+    return (
+        <p className="num-results">
+            <strong>{movies.length}</strong> resultados encontrados
+        </p>
+    )
+}
+
+export default Nav;
